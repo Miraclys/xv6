e157cb91f59654d7f3b7d730bdb65656ea9dde84
miraclys <miraclysyunsenl@gmail.com>
2024-09-13 01:38:43 +0800

    review pingpong implementation

--- user/pingpong.c
+++ user/pingpong.c
@@ -34,13 +34,14 @@ int main(int argc, char* argv[]) {
 		} else {
 			fprintf(1, "%d: received ping\n", getpid());
 		}
+		close(parent2child[READ]); // M: try to close here
 
 		if (write(child2parent[WRITE], &data, sizeof(char)) != sizeof(char)) {
 			fprintf(2, "[ERROR] child write error.");
 			flg = 1;
 		}
 
-		close(parent2child[READ]);
+		// close(parent2child[READ]);
 		close(child2parent[WRITE]);
 
 		exit(flg);
@@ -52,6 +53,7 @@ int main(int argc, char* argv[]) {
 			fprintf(2, "[ERROR] parent write error.");
 			flg = 1;
 		}
+		close(parent2child[WRITE]); // M: try to close here
 
 		// 这里会阻塞，等待子进程写入
 		if (read(child2parent[READ], &data, sizeof(char)) != sizeof(char)) {
@@ -62,7 +64,13 @@ int main(int argc, char* argv[]) {
 		}
 
 		close(child2parent[READ]);
-		close(parent2child[WRITE]);
+		// close(parent2child[WRITE]);
+
+		int status; // M: fix the logic of exit between parent and child
+		wait(&status);
+		if (status != 0) {
+			flg = 1;
+		}
 
 		exit(flg);
 	}
