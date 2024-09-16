9963ffdf74e62bd9b4039debf6ff01da97b886c1
miraclys <miraclysyunsenl@gmail.com>
2024-09-16 17:45:28 +0800

    review xargs implementation

--- user/xargs.c
+++ user/xargs.c
@@ -22,20 +22,27 @@ int main(int argc, char* argv[]) {
 
     int n;
     char temp[MAXN];
+    // M: read the input from stdin, store it in buf
     while ((n = read(0, buf, MAXN)) > 0) {
         int j = 0;
         for (int i = 0; i < n; ++i) {
+            // M: if the current character is '\n' or the last character in the buffer
             if (buf[i] == '\n' || i == n - 1) {
                 if (buf[i] != '\n') {
                     temp[j++] = buf[i];
                 }
-                temp[j] = '\0';
+                // M: the two kinds of implementation are the same
+                temp[j] = 0;
+                // temp[j] = '\0';
                 if (index < MAXARG - 1) {
                     // argvs[index] = strdup(temp);
+
+                    // M: add the new argument to the argvs
                     argvs[index] = temp; 
                     argvs[index + 1] = 0; 
                 }
                 if (fork() == 0) {
+                    // M: only add one argument to the argvs a time
                     exec(argv[1], argvs);
                     exit(1);
                 }
